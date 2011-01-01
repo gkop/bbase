7927a214d7d324b55069a1e4d62d95fa99eed492
Gabe Kopley <gabe@reframeit.com>
2011-01-01 18:40:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbase::Application.config.secret_token = '51df67af472558c4f3705fcae9bf058c1a0723814859ce77fb9734f1930da12bbecd43fbec344f90ca3ceaf44cd6f404f46e8979e518ed636dec1d47a35ca7ac'
