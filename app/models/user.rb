90b4018634a0c71b1b9a1377579fdd589134556a
Gabe Kopley <gabe@reframeit.com>
2011-01-01 20:18:23 -0500

    linked required scripts I18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User
   include Mongoid::Document
   field :name, :type => String
   field :email, :type => String
+  field :password, :type => String
+  field :password_confirmation, :type => String
   field :password_hash, :type => String
   field :password_reset, :type => String
 end
