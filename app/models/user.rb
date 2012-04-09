103d90c096a74a997a8ccad8ac09bd5c7ad77a42
Gabe Kopley <gabe@coshx.com>
2012-04-08 20:48:13 -0700

    s/DateTime/Time in devise user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,13 @@ class User
   field :email, :type => String
   field :encrypted_password, :type => String
   field :sign_in_count, :type => Integer, :default => 0
-  field :current_sign_in_at, :type => DateTime
-  field :last_sign_in_at, :type => DateTime
+  field :current_sign_in_at, :type => Time
+  field :last_sign_in_at, :type => Time
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip, :type => String
-  field :remember_created_at, :type => DateTime
+  field :remember_created_at, :type => Time
   field :invitation_token, :type => String
-  field :invitation_sent_at, :type => DateTime
+  field :invitation_sent_at, :type => Time
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
 
