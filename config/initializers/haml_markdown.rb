789f94b7895c5b5f2ea330dcc186a39ad4a078db
Gabe Kopley <gabe@coshx.com>
2013-01-01 20:53:08 -0800

    Enhance markdown image titles and captions, fixes #59

--- config/initializers/haml_markdown.rb
+++ config/initializers/haml_markdown.rb
@@ -3,6 +3,24 @@ module Haml::Filters::Markdown
   lazy_require "redcarpet"
 
   def render(text)
-    Redcarpet.new(text).to_html
+    html = Redcarpet.new(text).to_html
+    insert_captions(html)
+  end
+
+  private
+  def insert_captions(html)
+    parsed = Nokogiri::HTML(html)
+    parsed.css("img[title]").each do |img|
+      title = img['title']
+      title_inner_html = Redcarpet.new(img['title']).to_html
+      title_content =  Nokogiri.parse(title_inner_html).children[0]
+      span = Nokogiri::XML::Node.new "span", parsed
+      span.children = title_content.children
+      span.set_attribute("class", "image-caption")
+      img.add_next_sibling(span)
+      title.tr!('*', '"')
+      img["title"] = img["alt"] = title
+    end
+    parsed.to_s.html_safe
   end
 end
